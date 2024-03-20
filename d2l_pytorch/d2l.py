885b12a2453b3661640345f6877fb8572d5888b1
zhangyan <156013430@qq.com>
2024-03-20 21:33:05 +0800

    23.3.20 commit

--- d2l_pytorch/d2l.py
+++ d2l_pytorch/d2l.py
@@ -59,3 +59,52 @@ def load_data_fashion_mnist(batch_size: int):
     mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers
   )
   return train_iter, test_iter
+
+
+def evaluate_accuracy(data_iter, net):
+  acc_sum, n = 0.0, 0
+  for X, y in data_iter:
+    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+    n += y.shape[0]
+
+  return acc_sum / n
+
+
+def train_ch3(
+  net,
+  train_iter,
+  test_iter,
+  loss,
+  num_epochs,
+  batch_size,
+  params=None,
+  lr=None,
+  optimizer=None,
+):
+  for epoch in range(num_epochs):
+    train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+    for X, y in train_iter:
+      y_hat = net(X)
+      l1 = loss(y_hat, y).sum()
+
+      if optimizer is not None:
+        optimizer.zero_grad()
+      elif params is not None and params[0].grad is not None:
+        for param in params:
+          param.grad.data.zero_()
+
+      l1.backward()
+
+      if optimizer is None:
+        sgd(params, lr, batch_size)
+      else:
+        optimizer.step()
+      train_l_sum += l1.item()
+      train_acc_sum += (y_hat.argmax(dim=1) == y).sum().item()
+      n += y.shape[0]
+
+    test_acc = evaluate_accuracy(test_iter,net)
+    print(
+      "epoch %d , loss %.4f , train acc %.3f, test acc %.3f"
+      % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc)
+    )
