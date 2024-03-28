191f3698c2ea1deb66a28b938ca8be767db7cf6f
zhangyan <156013430@qq.com>
2024-03-28 21:22:06 +0800

    24.3.28 commit

--- d2l_pytorch/d2l.py
+++ d2l_pytorch/d2l.py
@@ -119,3 +119,12 @@ def train_ch3(
       "epoch %d , loss %.4f , train acc %.3f, test acc %.3f"
       % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc)
     )
+# 卷积运算
+def corr2d(X, K):
+  h, w = K.shape
+  Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+
+  for i in range(Y.shape[0]):
+    for j in range(Y.shape[1]):
+      Y[i, j] = (X[i : i + h, j : j + w] * K).sum()
+  return Y
